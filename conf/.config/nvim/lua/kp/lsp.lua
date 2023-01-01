f0f3b9672021ba931cf0c53f8ae0490c1558b310
Arthur Nikolaienko <anikolaienko@speroteck.com>
2023-01-01 15:07:42 +0200

    Lsp updates

--- conf/.config/nvim/lua/kp/lsp.lua
+++ conf/.config/nvim/lua/kp/lsp.lua
@@ -2,12 +2,29 @@ vim.opt.signcolumn = 'yes'
 
 local lsp = require('lsp-zero')
 
-lsp.preset('recommended')
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    setup_servers_on_start = true,
+    set_lsp_keymaps = true,
+    configure_diagnostics = true,
+    cmp_capabilities = true,
+    manage_nvim_cmp = true,
+    call_servers = 'local',
+    sign_icons = {
+        error = '',
+        warn = '',
+        hint = '',
+        info = ''
+    }
+})
 
 lsp.ensure_installed({
     'tsserver',
     'eslint',
-    'sumneko_lua'
+    'sumneko_lua',
+    'gopls',
+    'clangd',
+    'rust_analyzer',
 })
 
 lsp.nvim_workspace()
@@ -15,4 +32,3 @@ lsp.nvim_workspace()
 lsp.setup()
 
 vim.keymap.set('n', '<Leader>lf', ':LspZeroFormat<cr>')
-
--- conf/.config/nvim/plugin/packer_compiled.lua
+++ conf/.config/nvim/plugin/packer_compiled.lua
@@ -292,14 +292,14 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: Comment.nvim
-time([[Config for Comment.nvim]], true)
-try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
-time([[Config for Comment.nvim]], false)
 -- Config for: nvim-colorizer.lua
 time([[Config for nvim-colorizer.lua]], true)
 try_loadstring("\27LJ\2\n7\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\14colorizer\frequire\0", "config", "nvim-colorizer.lua")
 time([[Config for nvim-colorizer.lua]], false)
+-- Config for: Comment.nvim
+time([[Config for Comment.nvim]], true)
+try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
+time([[Config for Comment.nvim]], false)
 
 -- Command lazy-loads
 time([[Defining lazy-load commands]], true)
